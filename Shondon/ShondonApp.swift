511ac05e2c81f1eb29fa5bfaa92e36e23d5d59f3
Rashon Hyslop <shondon@Rashons-MacBook-Air.local>
2023-07-17 15:24:33 -0600

    Initial commit

---
+++ Shondon/ShondonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShondonApp.swift
+//  Shondon
+//
+//  Created by Rashon Hyslop on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ShondonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
